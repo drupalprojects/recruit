b949c424c202557fc1437c7a32c37a1bfb6e5fa3
Ivan Zugec <ivan@zugec.com>
2011-01-03 01:04:30 +0000

    Removed hardcoded application_ref field from the application module a3d7cc0

--- modules/application/theme/recruit-application.tpl.php
+++ modules/application/theme/recruit-application.tpl.php
@@ -2,13 +2,6 @@
 
 <div id="recruit-job-<?php print $job->job_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if($job_ref): ?>
-    <div class="field field-name-recruit-job-job-ref">
-      <div class="field-label"><?php print t('Job Reference'); ?>: </div>
-      <div class="field-item"><?php print $job_ref; ?></div>
-    </div>
-  <?php endif; ?>
-
   <?php print $content; ?>
 
   <?php if($changed): ?>
