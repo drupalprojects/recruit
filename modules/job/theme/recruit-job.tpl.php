84dcbe2f36f8b155036a030bfa3b2e2958a43cbd
Ivan Zugec <ivan@zugec.com>
2011-02-26 20:28:33 +1100

    Moved job over to git

--- modules/job/theme/recruit-job.tpl.php
+++ modules/job/theme/recruit-job.tpl.php
@@ -1,7 +1,4 @@
-<?php // $Id$ ?>
-
-<div id="recruit-job-<?php print $job->job_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
+<div id="recruit-job-<?php print $job->jid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if($job_ref): ?>
     <div class="field field-name-recruit-job-job-ref">
       <div class="field-label"><?php print t('Job Reference'); ?>: </div>
@@ -15,8 +12,7 @@
       <div class="field-item"><?php print $changed; ?></div>
     </div>
   <?php endif; ?>
-
-
-  <?php print $content; ?>
+  
+  <?php print render($content); ?>
 
 </div>
