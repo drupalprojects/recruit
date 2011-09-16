f0d17f42db8ea85237455bf44b2f4e3b7db2dfc5
Ivan Zugec <ivan@zugec.com>
2011-09-16 20:30:59 +1000

    Cleaned up code and removed old modules, ready for a fresh start.

--- modules/README.txt
+++
@@ -1,1 +0,0 @@
-Contains core Recruit modules.
--- modules/application/theme/recruit-application-form.tpl.php
+++
@@ -1,3 +0,0 @@
-<div id="recruit-application-form" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php print render($content); ?>
-</div>
